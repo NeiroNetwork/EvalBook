afc9ed1b3a62cdfc464bc8cb4258cf3490de3059
Nerahikada <hkshirorom@yahoo.co.jp>
2022-01-26 03:41:01 +0900

    Main: 最初にパーミッションを登録するように変更(エラーが出る)

--- src/NeiroNetwork/EvalBook/Main.php
+++ src/NeiroNetwork/EvalBook/Main.php
@@ -35,11 +35,10 @@ class Main extends PluginBase{
 
 	protected function onEnable() : void{
 		self::$instance = $this;
+		EvalBookPermissions::registerPermissions();
 		$this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
 		$this->operators = new Config(Path::join($this->getDataFolder(), "allowlist.txt"), Config::ENUM);
 		$this->getServer()->getCommandMap()->register($this->getName(), new EvalBookCommand("evalbook"));
-
-		EvalBookPermissions::registerPermissions();
 
 		CrashTracer::tryReadLastError();
 	}
