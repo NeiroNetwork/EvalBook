94addec22dafde1941b94f8cc09f6288384ba10e
SkyLake-git <70795425+SkyLake-git@users.noreply.github.com>
2022-03-07 22:13:58 +0900

    BookAddon - 仮追加

---
+++ src/NeiroNetwork/EvalBook/codesense/BookAddon.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\EvalBook\codesense;
+
+use pocketmine\item\WritableBookBase;
+
+abstract class BookAddon extends Addon { #abstract (temp)
+
+	#todo: EvalBookに別のEvalBookをインポートできる機能を追加
+
+	# 初期値の拡張など
+}
