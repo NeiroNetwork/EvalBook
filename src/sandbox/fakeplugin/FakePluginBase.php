dad15062dd4f868de66098929acab30090e9c99f
ねらひかだ <hkshirorom@yahoo.co.jp>
2025-06-13 16:58:48 +0900

    Update FakePluginBase.php
    https://github.com/pmmp/PocketMine-MP/commit/2a42e2c75d1c1b92d17559833899ce35940da269

--- src/sandbox/fakeplugin/FakePluginBase.php
+++ src/sandbox/fakeplugin/FakePluginBase.php
@@ -13,8 +13,7 @@ use Symfony\Component\Filesystem\Path;
 abstract class FakePluginBase extends PluginBase{
 
 	public static function create(string $name, string $version) : static{
-		return new static(new FakePluginLoader(),
-			Server::getInstance(),
+		return new static(Server::getInstance(),
 			new PluginDescription(["name" => $name, "version" => $version, "main" => static::class, "api" => ["6.0.0"]]),
 			Path::join(EvalBook::getPlugin()->getDataFolder(), "sandbox"),
 			uniqid("plugin_", true),
